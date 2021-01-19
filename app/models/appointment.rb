65feb3ddc0bc1530008c100ef7f0954b3026cac6
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-19 15:36:59 -0500

    validation adds

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,8 @@
 class Appointment < ApplicationRecord
     belongs_to :user
     belongs_to :service
+
+    validates :date, uniqueness: { scope: :time,
+    message: "Appointment is unavailable, please submit again." }
     
 end
